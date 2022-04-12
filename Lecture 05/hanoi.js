1983b9de324bc10540a9bb9e3829e36c286783c0
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2022-04-12 21:13:42 +0900

    [HG] 5강 업로드

---
+++ Lecture 05/hanoi.js
@@ -0,0 +1,22 @@
+/*var resetBtn = document.getElementById("resetBtn");
+var first = document.getElementById("first");
+var boxColor = document.getElementById("boxColor");
+
+var count = 0;
+
+resetBtn.addEventListener('click', function(){
+    if(this.onclick)
+    {
+        count =+ 1;
+    }
+    if(count == 0)
+    {
+        first.textContent = "block";
+        boxColor.textContent = "none";
+    }
+    if(count == 1)
+    {
+        first.textContent = "none";
+        boxColor.textContent = "block";
+    }
+})*/
