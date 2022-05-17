af2354d515078873fff6b98cef1ececf3b344318
uhbbang33 <55686424+uhbbang33@users.noreply.github.com>
2022-05-17 23:29:32 +0900

    [HG]

--- Canvas/canvas.js
+++ Canvas/canvas.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("game");
 var context = canvas.getContext('2d');
 
+class gamemanager{}
+
 class player
 {
     constructor(position_x, position_y, radius, color)
@@ -10,6 +12,7 @@ class player
         this.radius = radius;
         this.color = color;
     }
+
     draw()
     {
         context.beginPath();
@@ -20,11 +23,43 @@ class player
     }
 }
 
-canvas.addEventListener('click', function (e) {
-    var x1 = e.clientX - canvas.offsetLeft;
-    var y1 = e.clientY - canvas.offsetTop;        
-    console.log(x1, y1);
+class bullet{
+    constructor(position_x,position_y,radius,color){
+        this.position_x = position_x;
+        this.position_y = position_y;
+        this.radius = radius;
+        this.color = color;
+    }
+    draw()
+    {
+        context.beginPath();
+        context.arc(this.position_x, this.position_y, this.radius, 0, 2*Math.PI);
+        context.fillStyle = this.color;
+        context.fill();
+        context.closePath();
+    }
+}
 
+var char_player = new player(100,100,20,'skyblue');
+char_player.draw();
+
+var bullets = [];
+
+canvas.onclick = function(event){
+
+    let clickpos_x = event.clientX -context.canvas.offsetLeft;
+    let clickpos_y = event.clientY -context.canvas.offsetTop;
+
+    let bullets = new bullet(clickpos_x,clickpos_y,5,5,'pink');
+    bullets.draw();
+};
+
+// canvas.addEventListener('click', function (e) {
+//     var x1 = e.clientX - canvas.offsetLeft;
+//     var y1 = e.clientY - canvas.offsetTop;        
+//     console.log(x1, y1);
+// }
+// )
 /*canvas("click", function (e)
 {
     if(canvas.getContext)
